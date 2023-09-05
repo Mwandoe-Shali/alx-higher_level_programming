0374d194ca0760184e2e2075b2370cc8cc2416fb
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-06 00:58:18 +0300

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
 def print_last_digit(number):
     # Get the absolute value of the number to handle negative numbers
-    abs_number = abs(number)
+    absolute_number = abs(number)
 
     # Get the last digit using the modulo operator (%)
-    last_digit = abs_number % 10
+    last_digit = absolute_number % 10
 
     # Print the last digit
     print(last_digit)
