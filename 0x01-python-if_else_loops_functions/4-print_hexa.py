e9cf4fd00a4cb26a62d7d177682ad31b53533285
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-06 01:58:14 +0300

    Update 4-print_hexa.py

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-for num in range(0, 99):
-    #print(f"{num} = {hex(num)}")
-    print("{} = {}".format(num, hex(num)))
+for number in range(0, 99):
+    print('{} = 0x{:x}'.format(number, number))
