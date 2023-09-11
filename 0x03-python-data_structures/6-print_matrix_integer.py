45ef79f64ead19c1bacb4c5ea4285b129d35f608
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-11 15:42:40 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,7 +7,6 @@ def print_matrix_integer(matrix=[[]]):
             if col == row[-1]:
                 print('{:d}'.format(col), end='')
             else:
-                # If col is last element in row, print with a trailing space
                 print('{:d}'.format(col), end=' ')
         # Move to the next row after printing all columns
         print()
