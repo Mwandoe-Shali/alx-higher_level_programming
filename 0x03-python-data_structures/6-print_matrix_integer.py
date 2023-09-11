77c206b635d6128f8ac0eba4b70eadef97347da2
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-11 15:38:32 +0300

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,8 +5,10 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for col in row:
             if col == row[-1]:
-                # If col is not the last element in the row,
-                # print without a trailing space
+                '''
+                If col is not the last element in the row,
+                print without a trailing space
+                '''
                 print('{}'.format(col), end='')
             else:
                 # If col is last element in row, print with a trailing space
