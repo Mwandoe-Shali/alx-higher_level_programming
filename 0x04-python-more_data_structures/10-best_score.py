84028b982fd617bbc375c1dae67acdbb2252c04e
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-09-14 02:26:21 +0300

    Create 10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """returns a key with the biggest integer value
+    Args:
+        a_dictionary: the dictionary
+    Returns:
+        a key with the biggest integer value
+    """
+    return max(a_dictionary, key=a_dictionary.get) if a_dictionary else None
