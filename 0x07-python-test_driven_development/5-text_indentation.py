5a976e80b3e926b9b1a1aea87562b10a2b56e2c8
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-10-06 23:50:06 +0300

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -5,8 +5,8 @@ Module 5-text_indentation
 
 
 def text_indentation(text):
-    ''' This function prints a text with 2 new lines after each ".", "?", or ":"
-    There should be no space at the beginning or at the end of each printed line
+    ''' Function prints a text with 2 new lines after each ".", "?", or ":"
+    No space at the beginning or at the end of each printed line
 
     Args:
         text (str): The string to be printed
