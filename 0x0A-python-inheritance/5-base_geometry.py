05a0348f34363d74764a2ac491a316ad59cd9cb3
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-10-10 05:30:56 +0300

    Create 5-base_geometry.py

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+This module defines the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Empty class BaseGeometry.
+    """
+    pass
