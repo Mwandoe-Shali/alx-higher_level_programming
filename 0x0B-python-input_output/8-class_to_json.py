9c6e42cf30b40903eb811a8e7a3352f8e7f6014c
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-10-11 01:25:06 +0300

    Create 8-class_to_json.py

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module: 8-class_to_json
+Returns the dictionary description with simple data structure 
+for JSON serialization of an object.
+"""
+
+
+def class_to_json(obj):
+    """
+    Args:
+        obj: An instance of a class.
+
+    Returns:
+        dict: The dictionary description of the object.
+    """
+    return obj.__dict__
