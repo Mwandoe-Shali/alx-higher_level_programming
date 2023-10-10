6083c7556fa531d74dcb71f67fbc5a777d3e64c9
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2023-10-11 01:46:06 +0300

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module: 8-class_to_json
-Returns the dictionary description with simple data structure 
+Returns the dictionary description with simple data structure
 for JSON serialization of an object.
 """
 
