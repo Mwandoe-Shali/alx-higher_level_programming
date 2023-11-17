b0579f9ae379869b70920d99e231e029a6e2c038
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-11-18 01:12:51 +0300

    ALX no wahala, doing on third deadline

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+-- Creates the database hbtn_0c_0 in your MySQL server.
+-- If it exists, it shouldn't fail
+-- Don't use SELECT / SHOW
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- Deletes the database hbtn_0c_0 in your MySQL server.
+-- If it exists, it shouldn't fail
+-- Don't use SELECT / SHOW
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
