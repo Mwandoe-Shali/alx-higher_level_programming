3c119970db5eb67f623d5bc13135719cd7e05e57
Mwandoe-Shali <mwandoeshali@gmail.com>
2023-11-19 02:05:19 +0300

    Shut Down - BlackPink

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -9,8 +9,8 @@
 
 SELECT tg.name AS genre
         COUNT(*) AS number_of_shows
-FROM tv_genres tg
-INNER JOIN tv_show_genres tsg
+FROM tv_genres AS tg
+INNER JOIN tv_show_genres AS tsg
 ON tg.id = tsg. genre_id
 GROUP BY tg.name
 ORDER BY number_of_shows DESC;
