b36556e24341a27dc6a2c2641ee5049c9ced267c
Mwandoe Shali <117778682+Mwandoe-Shali@users.noreply.github.com>
2024-01-08 20:09:46 +0300

    Create 2-rectangle.js

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/**
+ * Check the parameters provided
+ * If w or h is equal to 0 or not a
+ *            positive integer, create an empty object
+ */
+
+class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && w > 0 && typeof h === 'number' && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
